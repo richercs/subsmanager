e71cd00c5382998761c8cc1f1f90a8e84f488014
Richer Csaba <richercsaba@gmail.com>
2017-02-24 14:06:30 +0100

    Set session event id automatically for attendance records

--- src/AppBundle/Form/AttendanceHistoryType.php
+++ src/AppBundle/Form/AttendanceHistoryType.php
@@ -14,7 +14,6 @@ class AttendanceHistoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('session_event')
             ->add('attendee')
             ->add('subscription')
         ;
