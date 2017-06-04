75e887b6d25662cbff51bd66ab6b0f9e442bc5ed
Richer Balázs <richer.balazs@gmail.com>
2017-06-04 19:20:55 +0200

    Dedicated LogoutSuccessHandler for additional steps after login:
     - redirect to main appilcation homepage.

---
+++ src/AppBundle/Controller/LogoutHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+
+class LogoutHandler implements LogoutSuccessHandlerInterface
+{
+    /**
+     * Creates a Response object to send upon a successful logout.
+     *
+     * @param Request $request
+     *
+     * @return Response never null
+     */
+    public function onLogoutSuccess(Request $request)
+    {
+        return new RedirectResponse('http://tornazzvelem.dev');
+    }
+}
