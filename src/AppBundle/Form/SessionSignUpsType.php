078850eeb5a9e1810737407b8f7db9400c96e615
Richercs <richercsaba@gmail.com>
2020-05-31 13:11:34 +0200

    Added basic symfony forms for the two new entites

---
+++ src/AppBundle/Form/SessionSignUpsType.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace AppBundle\Form;
+
+
+use PUGX\AutocompleterBundle\Form\Type\AutocompleteType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionSignUpsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('userAccount', AutocompleteType::class, array(
+                'class' => 'AppBundle:UserAccount'
+            ))
+//            ->add('subscription')
+//            ->add('subscription_info', TextareaType::class)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\SessionSignUps'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'SessionSignUpsType';
+    }
+
+}
