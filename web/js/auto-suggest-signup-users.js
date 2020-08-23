990ac521427d19b527d77735ea70a7d138c0a43a
Richercs <richercsaba@gmail.com>
2020-08-23 11:50:18 +0200

    Removed commented out lines from js script

--- web/js/auto-suggest-signup-users.js
+++ web/js/auto-suggest-signup-users.js
@@ -7,21 +7,10 @@ $(document).ready(function () {
         allow_down: false,
         add_at_the_end: true,
         after_add: function (collection, element) {
-            // $('.subscription-info-wrap').find('textarea').attr('disabled', true);
-            // element.find('select').on('change', function () {
-            //     var recordNumber = parseInt(this.id.replace(/[^0-9\.]/g, ''), 10);
-            //     loadSubscriptionInfo(this.value, recordNumber);
-            // });
             // after adding a new record the form the new input field gets the autocompleter functionality
             element.find('input[type=text]').autocompleter({
                 url_list: '/useraccount_search',
                 url_get: '/useraccount_get/',
-                // on_select_callback: function (item) {
-                //     var recordNumber = parseInt(item.attr('id').replace(/[^0-9\.]/g, ''), 10);
-                //     if (!isNaN(recordNumber)) {  // Only load subscriptions on session event handling
-                //         loadSubscriptionRecord(item.val(), recordNumber);
-                //     }
-                // }
             });
         }
     });
@@ -32,46 +21,8 @@ $(document).ready(function () {
         url_list: '/useraccount_search',
         url_get: '/useraccount_get/'
     });
-    //
-    // function loadSubscriptionRecord(id, recordNumber) {
-    //     $.ajax({
-    //         url: '/loadSubscription',
-    //         data: {
-    //             owner_id: id
-    //         },
-    //         success: function (data) {
-    //             var subscriptionFiledId = "#appbundle_sessionevent_attendees_".concat(recordNumber, '_subscription');
-    //
-    //             $(subscriptionFiledId).find('option').not('option:first').remove();
-    //
-    //             $.each(data, function (key, value) {
-    //
-    //                 var selectedText = value['is_owned'] ? 'selected' : '';
-    //
-    //                 $(subscriptionFiledId)
-    //                     .append($("<option " + selectedText + " ></option>")
-    //                         .attr("value", value['id'])
-    //                         .text(value['label']));
-    //             });
-    //
-    //             $.each(data, function (key, value) {
-    //               if (value['is_owned']) {
-    //                   loadSubscriptionInfo(value['id'], recordNumber);
-    //               }
-    //             });
-    //         }
-    //     });
-    // }
-
-    // $('.subscriptions-input-wrap').find('select').attr('disabled', true);
-    // $('.subscription-info-wrap').find('textarea').attr('disabled', true);
 
     var form = $('.form-horizontal');
-    //
-    // form.submit(function () {
-    //     $('.subscriptions-input-wrap').find('select').prop("disabled", false);
-    //     $('.subscription-info-wrap').find('textarea').prop("disabled", false);
-    // });
 
     var pathname = window.location.pathname; // Returns path only
 
@@ -83,30 +34,6 @@ $(document).ready(function () {
             $("#appbundle_announced_session_scheduleItem").prop("disabled", false);
         });
     }
-    //
-    // function loadSubscriptionInfo(id, recordNumber) {
-    //     $.ajax({
-    //         url: '/loadSubscriptionInfo',
-    //         data: {
-    //             subscription_id: id
-    //         },
-    //         success: function (data) {
-    //
-    //             var subscriptionInfo = "#appbundle_sessionevent_attendees_".concat(recordNumber, '_subscription_info');
-    //
-    //             if (data['id'] != null) {
-    //                 if (data['attendance_limit'] == null) {
-    //                     $(subscriptionInfo).val("Havi bérlet (használatok száma: " + data['attendance_count'] + ")");
-    //                 } else {
-    //                     $(subscriptionInfo).val("Alkalmak Száma: " + data['attendance_limit']
-    //                         + "\n"
-    //                         + "Fennmaradó: " + data['attendance_left']);
-    //                 }
-    //             }
-    //         }
-    //     });
-    // }
-
 });
 
 
